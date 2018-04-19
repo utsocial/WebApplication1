766883049dbf86b903c3bfbf70f6c2adce2ff8db
utsocials <utsocials@gmail.com>
2018-04-19 15:40:47 -0700

    First Commit

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+using WebApplication1.Services;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ICountriesRepositoryInMemory CountriesRepositoryInMemory { get; }
+
+        public HomeController(ICountriesRepositoryInMemory countriesRepositoryInMemory)
+        {
+            CountriesRepositoryInMemory = countriesRepositoryInMemory;
+        }
+
+        public IActionResult Index()
+        {
+            var hardCodedContries = CountriesRepositoryInMemory.getCountries();
+            return View(hardCodedContries);
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ WebApplication1/Services/ContriesRepositoryInMemory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Services
+{
+    public class ContriesRepositoryInMemory : ICountriesRepositoryInMemory
+    {
+        public List<string> getCountries()
+        {
+            List<string> hardCodedContries = new List<string>() { "US", "Mexico", "Colombia" };
+            return hardCodedContries;
+        }
+    }
+}
